8dd16ebcb5cf846c8187e4e74fda8731cf45cf5f
KJ <grazerjink@gmail.com>
2017-11-25 17:36:03 +0700

    Build UI

--- TourDuLich.Web/Controllers/HomeController.cs
+++ TourDuLich.Web/Controllers/HomeController.cs
@@ -11,5 +11,19 @@ namespace TourDuLich.Web.Controllers
         {
             return View();
         }
+        
+        [Route("dang-ky")]
+        // GET: Home
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        
+        [Route("thong-ke")]
+        // GET: Home
+        public ActionResult ThongKe()
+        {
+            return View();
+        }
     }
 }
